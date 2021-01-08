7a32d22f4ceded3d3fec838f2d6505d1b8b1a1b5
Ilya Kavalerov <ilyakavalerov@gmail.com>
2021-01-08 17:26:50 -0500

    Fixes? TPU type issue

--- models.py
+++ models.py
@@ -159,10 +159,10 @@ class DPT_base(nn.Module):
 
         rest = segment_size - (segment_stride + seq_len % segment_size) % segment_size
         if rest > 0:
-            pad = Variable(torch.zeros(batch_size, dim, rest, dtype=input.dtype).to(input.device))
+            pad = Variable(torch.zeros(batch_size, dim, rest, dtype=input.dtype)).to(input.device)
             input = torch.cat([input, pad], 2)
 
-        pad_aux = Variable(torch.zeros(batch_size, dim, segment_stride, dtype=input.dtype).to(input.device))
+        pad_aux = Variable(torch.zeros(batch_size, dim, segment_stride, dtype=input.dtype)).to(input.device)
         input = torch.cat([pad_aux, input, pad_aux], 2)
 
         return input, rest
